13d069751046656cd6a551d454bcb9df5ab1523f
marex980 <69250892+marex980@users.noreply.github.com>
2023-03-29 14:20:31 +0200

    newsletter

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,9 +1,29 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react";
+import Layout from "../components/Layout";
 const NewsLetter = () => {
   return (
-    <h4>newsletter</h4>
-  )
-}
+    <Layout className="newsletter-page">
+      <section className="page-center">
+        <h2>Get all the latest stories to your inbox</h2>
+        <h4>I write to my friends every few weeks</h4>
+        <form className="contact-form">
+          <input
+            type="text"
+            name="name"
+            placeholder="Type your name"
+            className="form-control"
+          ></input>
+                    <input
+            type="email"
+            name="email"
+            placeholder="Type your email address"
+            className="form-control"
+          ></input>
+          <button type='submit' className='btn form-control submit-btn'>subsribe</button>
+        </form>
+      </section>
+    </Layout>
+  );
+};
 
-export default NewsLetter
+export default NewsLetter;
